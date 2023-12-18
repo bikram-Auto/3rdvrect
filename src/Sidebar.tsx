7ec43af3379d98bc8d458221260da141a3a61139
Bikram <bikram.nanda@squbix.com>
2023-12-18 11:45:48 +0530

    create side bar

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -5,20 +5,21 @@ import { cn } from './utils/util';
 import { DiAtom } from "react-icons/di";
 import { LiaHomeSolid } from "react-icons/lia";
 import { RiArrowLeftDoubleLine } from "react-icons/ri";
+import { Link } from 'react-router-dom';
 
 type props = {}
 
 interface SideNavItemsType{
-    icon:{
+    icons?:{
         icon: React.ReactNode;
         fillIcon: React.ReactNode;
     };
     label: string;
-    heref: string;
+    href: string;
 }
 
 export default function Sidebar(){
-    const [isSidebarOpen, setisSidebarOpen] = useState(true)
+    const [isSidebarOpen, setSidebarOpen] = useState(true)
 
     return(
         <div>
@@ -26,38 +27,52 @@ export default function Sidebar(){
                 "min-h-screen max-h-screen overflow-y-auto w-fit md:pr-10 pr-3 pt-2 flex flex-col gap-3 border-r-[2px] pl-[50px]")}>
                 <HoverContainer>
                     <div className="p-2 pb-4 flex justify-between items-center">
-                    <button>
-                            <link rel="/" href="Home" className="button-style"/>
+                        <button>
                             <DiAtom className='text-7xl' /> 
                         </button>
                     </div>
                 </HoverContainer>
                 <HoverContainer>
-                    <SideNavItems/>
+                    <SideNavItems icons={{
+                        fillIcon: <LiaHomeSolid />,
+                        icon: <LiaHomeSolid />
+                    }}
+                    href="/"
+                    label="Home" 
+                    isSidebarOpen={isSidebarOpen}/>
                 </HoverContainer>  
                  {/* toggle button */}
-            <HoverContainer>
-                <RiArrowLeftDoubleLine
-                onClick={()=>setisSidebarOpen(!isSidebarOpen)}
-                 className={cn('text-gray-400 transition-all text-4xl items-center', !isSidebarOpen && 'rotate-180')} />
-            </HoverContainer>             
+                <HoverContainer>
+                    <RiArrowLeftDoubleLine
+                    onClick={()=>setSidebarOpen(!isSidebarOpen)}
+                    className={cn(
+                        'text-gray-400 transition-all text-4xl items-center', !isSidebarOpen && 'rotate-180'
+                        )} 
+                    />
+                </HoverContainer>             
             </div>
            
         </div>
     )
 }
 
-function SideNavItems(){
-    return <div className='flex gap-2 items-center cursor-pointer'>
+function SideNavItems({href, isSidebarOpen, icons, label}: SideNavItemsType & {isSidebarOpen: boolean}){
+    return (
+        <Link to={href} className='flex gap-2 items-center cursor-pointer'>
 
-        {/* icon */}
-        <div className='w-[35px] h-[35px] text-3xl'>
-            <LiaHomeSolid />
-        </div>
-        {/* lebel */}
-        <p className='text-xl hidden md:block transition-all'>Home</p>
-        
-    </div>
+            {/* icon */}
+            <div className='w-[35px] h-[35px] text-3xl'>
+                {icons?.icon}
+                
+            </div>
+            {/* lebel */}
+            {isSidebarOpen && (
+            <p className='text-xl hidden md:block transition-all'>
+                {label}</p>
+            )}
+            
+        </Link>
+    );
 }
 
 function HoverContainer({
@@ -68,5 +83,6 @@ function HoverContainer({
         className?: string;
     }){
         return <div className={
-            'p-3 transition-all rounded-full cursor-pointer hover:bg-gray-200 dark:hover:bg-zinc-900 group-hover:dark:bg-zinc-900 group-hover:bg-gray-200'}>{children}</div>;
+            "p-3 transition-all rounded-full cursor-pointer hover:bg-gray-200 dark:hover:bg-zinc-900 group-hover:dark:bg-zinc-900 group-hover:bg-gray-200"}>
+                {children}</div>;
     }
