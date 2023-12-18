4749243561911b65516f5e64b699a1daa182c4f6
Bikram <bikram.nanda@squbix.com>
2023-12-18 17:08:44 +0530

    complite side nav items

--- src/Sidebar.tsx
+++ src/Sidebar.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-import React, { useState } from 'react';
+import React, {useState } from 'react';
 import { cn } from './utils/util';
-import { DiAtom } from "react-icons/di";
-import { LiaHomeSolid } from "react-icons/lia";
+import { IoMdSnow } from "react-icons/io";
 import { RiArrowLeftDoubleLine } from "react-icons/ri";
 import { Link } from 'react-router-dom';
+import { GoHomeFill, GoHome } from "react-icons/go";
+import { useLocation } from 'react-router-dom';
+import { FcDataConfiguration } from "react-icons/fc";
+import { IoLockClosedOutline, IoLockClosed } from "react-icons/io5";
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
+import './assets/logo.svg'
+
+// const logo= require("./assets/logo.svg")
 type props = {}
 
 interface SideNavItemsType{
@@ -18,59 +25,102 @@ interface SideNavItemsType{
     href: string;
 }
 
-export default function Sidebar(){
-    const [isSidebarOpen, setSidebarOpen] = useState(true)
+const sidebarItems: SideNavItemsType[] = [
+    {
+        href: "/",
+        label: "Home",
+        icons: {
+            fillIcon: <GoHomeFill />,
+            icon: <GoHome />
+        }
+    },
+    {
+        href: "/configuration",
+        label: "Configuration",
+        icons: {
+            fillIcon: <FcDataConfiguration />,
+            icon: <FcDataConfiguration />
+        }
+    },
+    {
+        href: "/authorization",
+        label: "Autorization",
+        icons: {
+            fillIcon: <IoLockClosed />,
+            icon: <IoLockClosedOutline />
+        }
+    }
 
+]
+
+export default function Sidebar(){
+    const [isSidebarOpen, setSidebarOpen] = useState(true);
     return(
-        <div>
+        <div className="items-center">
             <div className={cn(
-                "min-h-screen max-h-screen overflow-y-auto w-fit md:pr-10 pr-3 pt-2 flex flex-col gap-3 border-r-[2px] pl-[50px]")}>
+                "min-h-screen max-h-screen overflow-y-auto w-fit md:pr-2 pr-3 pt-2 flex flex-col gap-3 border-r-[3px] pl-[20px]",
+                isSidebarOpen && "md:w-[300px]"
+                )}>
                 <HoverContainer>
-                    <div className="p-2 pb-4 flex justify-between items-center">
-                        <button>
-                            <DiAtom className='text-7xl' /> 
+                    <div className="p-1 pb-2 flex justify-between ">
+                        <button>                           
+                            <IoMdSnow className='text-7xl ' /> 
                         </button>
                     </div>
                 </HoverContainer>
-                <HoverContainer>
-                    <SideNavItems icons={{
-                        fillIcon: <LiaHomeSolid />,
-                        icon: <LiaHomeSolid />
-                    }}
-                    href="/"
-                    label="Home" 
-                    isSidebarOpen={isSidebarOpen}/>
-                </HoverContainer>  
+                {/* Side Nav Itemes */}
+                {sidebarItems.map((d, i) => (
+                    <HoverContainer key={i}>
+                        <SideNavItems 
+                            icons= {d.icons}
+                            href= {d.href}
+                            label= {d.label} 
+                            isSidebarOpen={isSidebarOpen}/>
+                    </HoverContainer>
+                    ))}
                  {/* toggle button */}
-                <HoverContainer>
-                    <RiArrowLeftDoubleLine
-                    onClick={()=>setSidebarOpen(!isSidebarOpen)}
-                    className={cn(
-                        'text-gray-400 transition-all text-4xl items-center', !isSidebarOpen && 'rotate-180'
-                        )} 
-                    />
-                </HoverContainer>             
+                 <section className={cn(
+                    "flex w-full justify-end",
+                    !isSidebarOpen && "justify-start"
+                 )}>
+                    <HoverContainer>
+                        <RiArrowLeftDoubleLine
+                        onClick={()=>setSidebarOpen(!isSidebarOpen)}
+                        className={cn(
+                            'text-gray-400 transition-all text-4xl items-center', !isSidebarOpen && 'rotate-180'
+                            )} 
+                        />
+                    </HoverContainer> 
+                </section>            
             </div>
            
         </div>
     )
 }
 
-function SideNavItems({href, isSidebarOpen, icons, label}: SideNavItemsType & {isSidebarOpen: boolean}){
+function SideNavItems({
+    href, 
+    isSidebarOpen, 
+    icons, 
+    label
+}: SideNavItemsType & {isSidebarOpen: boolean}){
+    const [animationParent] = useAutoAnimate()
+    const pathName = useLocation().pathname; 
+    const isActivePage = pathName == href;
     return (
-        <Link to={href} className='flex gap-2 items-center cursor-pointer'>
-
+        <Link ref={animationParent} to={href} className='flex gap-2 items-center cursor-pointer'>
             {/* icon */}
-            <div className='w-[35px] h-[35px] text-3xl'>
-                {icons?.icon}
-                
+            <div className='w-[35px] h-[35px] text-3xl'>           
+                {isActivePage ? icons?.fillIcon : icons?.icon}           
             </div>
             {/* lebel */}
             {isSidebarOpen && (
-            <p className='text-xl hidden md:block transition-all'>
-                {label}</p>
+            <p className={cn("text-xl hidden md:block transition-all",
+            isActivePage&& "font-bold"
+            )}>
+                { label}
+            </p>
             )}
-            
         </Link>
     );
 }
