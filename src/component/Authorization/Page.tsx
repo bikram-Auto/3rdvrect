37884ba0f0c491176e752724f26ef07426bc8372
Bikram <bikram.nanda@squbix.com>
2023-12-20 18:04:39 +0530

    create Authontication, configuration, Home page

---
+++ src/component/Authorization/Page.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+
+export default function AuthorizationPage() {
+  return (
+    <h1 className='items-start ml-[500px]  font-bold'>
+    Authorization page</h1>
+  );
+}
